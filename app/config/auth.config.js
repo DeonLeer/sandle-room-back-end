868f43165f8ddc7410cc877636a3e6a85ff1248e
DeonLeer <deon.leer96@gmail.com>
2022-10-04 14:10:38 -0700

    connected db. set up auth w/ jwt & bcrypt

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "sandle-feet-bauer-autumn"
+  };
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
 const app = express();
 
 app.use(cors());
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require('./app/models');
+
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
   });
+  
+
+function initial() {
+Role.create({
+    id: 1,
+    name: "user"
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+Role.create({
+    id: 2,
+    name: "admin"
+});
+}
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+  });
+
+app.listen(8080, () =>
+    console.log("API is running on http://localhost:8080")
+);
