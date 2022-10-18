b53de517b47f68ffbd3c6a0c8eba268161086ba7
DeonLeer <deon.leer96@gmail.com>
2022-10-17 23:48:16 -0700

    more availability routes and functions

--- app/controllers/availability.controller.js
+++ app/controllers/availability.controller.js
@@ -11,16 +11,50 @@ exports.getDefaultAvailability = (req, res) => {
     });
 }
 
-exports.modifyDefaultAvailabilities = (req, res) => {
+exports.createDefaultAvailability = (req, res) => {
+  connection.query('INSERT INTO default_availability (hostId, startTime, endTime, weekday) VALUES (?, ?, ?, ?)', [req.userId, req.body.startTime, req.body.endTime, req.body.weekday], (err, result) => {
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.status(200).send(result)
+    }
+  })
+}
+
+exports.editDefaultAvailability = (req, res) => {
+
   res.status(200)
+}
+
+exports.deleteDefaultAvailability = (req, res) => {
+  
+  connection.query(`DELETE FROM default_availability WHERE id = ? AND hostUserId = ${req.userId}`, [req.body.id], (err, result) => {
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.status(400).send(result)
+    }
+  })
 }
 
 exports.getAvailabilitiesByMonth = (req, res) => {
-  res.status(200)
+  connection.query('SELECT * FROM availability WHERE MONTH(startTime) = ? AND YEAR(startTime) = ? ', [req.body.month, req.body.year], (err, result) => {
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.status(200).send(result)
+    }
+  })
 }
 
 exports.createAvailability = (req, res) => {
-  res.status(200)
+  connection.query('INSERT INTO availability (hostId, startTime, endTime, available) VALUES (?, ?, ?, ?)', [req.userId, req.body.startTime, req.body.endTime, req.body.available], (err, result) => {
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.status(200).send(result)
+    }
+  })
 }
 
 exports.editAvailability = (req, res) => {
@@ -28,6 +62,13 @@ exports.editAvailability = (req, res) => {
 }
 
 exports.deleteAvailability = (req, res) => {
-  res.status(200)
+
+  connection.query(`DELETE FROM availability WHERE id = ? AND hostUserId = ${req.userId}`, [req.body.id], (err, result) => {
+    if (err) {
+      res.status(400).send(err)
+    } else {
+      res.status(400).send(result)
+    }
+  })
 }
 
--- app/routes/availability.routes.js
+++ app/routes/availability.routes.js
@@ -12,7 +12,11 @@ module.exports = function (app) {
 
   app.get('/api/availability/default', [authJwt.verifyToken], controller.getDefaultAvailability)
 
-  app.patch('api/availability/default', [authJwt.verifyToken], controller.modifyDefaultAvailabilities)
+  app.post('/api/availability/default', [authJwt.verifyToken], controller.createDefaultAvailability)
+
+  app.patch('api/availability/default', [authJwt.verifyToken], controller.editDefaultAvailability)
+
+  app.delete('/api/availability/default', [authJwt.verifyToken], controller.deleteDefaultAvailability)
 
   app.get('/api/availability', [authJwt.verifyToken], controller.getAvailabilitiesByMonth)
 
